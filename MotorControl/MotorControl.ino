e78d17e524b16553ceac1f339dd70b6e0bcb6318
cesar-moya <odin.dark@gmail.com>
2020-09-04 16:05:56 -0500

    fix speed for 24 v adapter

--- MotorControl/MotorControl.ino
+++ MotorControl/MotorControl.ino
@@ -113,8 +113,8 @@ bool autoLowerActivated = false;
 
 //Using custom values to ensure no more than 24v are delivered to the motors given my desk load.
 //feel free to play with these numbers but make sure to stay within your motor's rated voltage.
-const int PWM_SPEED_UP = 250; //0 - 255, controls motor speed when going UP
-const int PWM_SPEED_DOWN = 230; //0 - 255, controls motor speed when going DOWN
+const int PWM_SPEED_UP = 255; //0 - 255, controls motor speed when going UP
+const int PWM_SPEED_DOWN = 240; //0 - 255, controls motor speed when going DOWN
 
 void setup() {
   Serial.begin(9600);
